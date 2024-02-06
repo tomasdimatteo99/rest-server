feb9489b19ee7aa67503158ae0ff7515a5d1d460
Tomás Di Matteo <dimatteotomas99@gmail.com>
2024-02-05 22:41:21 -0300

    Update server.js
    Now don't need .env.

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 class Server {
     constructor() {
         this.app = express()
-        this.port = process.env.PORT
+        this.port = process.env.PORT || 8080
         this.usersPath = '/api/users'
 
         //Middlewaares - Functions that will add functionality to our webserveer.
